73fa581708186d3d782d5c8fb9b08a224ea7da6b
Edilson3012 <edilsongotardi@live.com>
2024-06-19 22:10:23 -0300

    Initial commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'role_id' => 1,
+            'name' => 'Admin User',
+            'email' => 'admin@teste.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::factory()->create([
+            'role_id' => 2,
+        ]);
+    }
+}
